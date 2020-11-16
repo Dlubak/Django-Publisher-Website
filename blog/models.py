1893b70927da56a5bfbcc37badb88adb2f932423
EU\dlubajac <jacekdlubak07@gmail.com>
2020-11-16 13:15:11 +0100

    Design models for blog application, register models on admin site, refactor index view to show articles

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=25)
+    body = models.TextField
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return self.body
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Article, Category
 
 
 def index(request):
-    return render(request, 'blog/index.html', {})
+    articles = Article.objects.all()
+    template_name = 'blog/index.html'
+    context = {'articles': articles}
+    return render(request, 'blog/index.html', context)
+
+
+def article_detail(request, article_id):
+    pass
