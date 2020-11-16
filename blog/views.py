30f9f2aa30c3e55a1f3927e5340ece495350405f
EU\dlubajac <jacekdlubak07@gmail.com>
2020-11-16 14:04:35 +0100

    create form to add new articles

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .models import Article, Category
+from .forms import ArticleForm
 
 
 def index(request):
@@ -18,5 +19,15 @@ def article(request, article_id):
     return render(request, template_name, context)
 
 
-def new article(request):
+def new_article(request):
     template_name = 'blog/new_article.html'
+    if request.method != 'POST':
+        articleForm = ArticleForm()
+    else:
+        articleForm = ArticleForm(request.POST)
+        if articleForm.is_valid():
+            new_article = articleForm.save(commit=False)
+            new_article.save()
+            return redirect('blog:article', new_article.id)
+    context = {'form': articleForm}
+    return render(request, 'blog/new_article.html', context)
