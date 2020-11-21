6980d9ccdb5da42b602f699773937a6bcf355a8f
Dlubak <jacekdlubak07@gmail.com>
2020-11-21 20:41:52 +0100

    add authentication decorator for editing articles to check if author is owner of this article

--- authentication/decorators.py
+++ authentication/decorators.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from django.shortcuts import redirect
+from blog.models import Article
+from django.core.exceptions import PermissionDenied
 
 def unauthenticated_user(view_func):
     @wraps(view_func)
@@ -8,4 +10,15 @@ def unauthenticated_user(view_func):
             return redirect('blog:index')
         else:
             return view_func(request, *args, **kwargs)
+    return wrap
+
+def check_owner_or_admin(view_func):
+    @wraps(view_func)
+    def wrap(request, *args, **kwargs):
+        user = request.user
+        article = Article.objects.get(pk=kwargs['article_id'])
+        if article.author == user or user.is_superuser:
+            return view_func(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
     return wrap
