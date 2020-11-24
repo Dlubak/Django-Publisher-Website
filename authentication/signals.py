146b7e789dd94b2f90f407813809c85737dbf8c8
Dlubak <jacekdlubak07@gmail.com>
2020-11-24 22:13:34 +0100

    fix

--- authentication/signals.py
+++ authentication/signals.py
@@ -14,12 +14,7 @@ def create_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-
 
 # def create_user_profile(sender, instance, created, **kwargs):
 #     """
